[fix] mask tensor broadcasting issues

joeynmt/batch.py
@@ -79,14 +79,15 @@ class Batch:
                 batch_size,seq_length = self.trg_input.shape
                 vocab_size = token_tags.shape[0]
                 
-                tag_dict = {i:x.item() for i,x in enumerate(token_tags[:,1])}
+                # tag_dict = {i:x.item() for i,x in enumerate(token_tags[:,1])}
                 
-                trg_tags = torch.tensor([tag_dict[x.item()] for x in self.trg_input.flatten()]).reshape(batch_size,seq_length)
+                # trg_tags = torch.tensor([tag_dict[x.item()] for x in self.trg_input.flatten()]).reshape(batch_size,seq_length)
                 
-                self.mask_tensor: Tensor = torch.empty((batch_size, seq_length, vocab_size), dtype=bool)
-                for b in range(batch_size):
-                    for s in range(seq_length):
-                        self.mask_tensor[b,s] = token_masks[trg_tags[b,s].item()]
+                # self.mask_tensor: Tensor = torch.empty((batch_size, seq_length, vocab_size), dtype=bool)
+                # for b in range(batch_size):
+                #     for s in range(seq_length):
+                #         self.mask_tensor[b,s] = token_masks[trg_tags[b,s].item()]
+                self.mask_tensor = token_masks[token_tags[self.trg_input.flatten()]].view(batch_size, seq_length, vocab_size)
             else:
                 self.mask_tensor = None
 

joeynmt/datasets.py
@@ -157,7 +157,7 @@ class BaseDataset(Dataset):
             
         # load tag list and mask tensors
         if self.split == "train":
-            token_tags = torch.load(self.tag_file)
+            token_tags = torch.load(self.tag_file)[:,1]
             token_masks = torch.load(self.mask_file)
         else:
             token_tags = None

joeynmt/training.py
@@ -213,7 +213,7 @@ class TrainManager:
         self.token_masks = torch.load(cfg["data"]["trg"]["mask_file"])
         
         # set theta for training penalty
-        self.theta = theta
+        self.theta = torch.tensor(theta, dtype=self.dtype).to(self.device)
 
     def _save_checkpoint(self, new_best: bool, score: float) -> None:
         """
@@ -371,7 +371,7 @@ class TrainManager:
         """
         assert path is not None
         layer_state_dict = OrderedDict()
-        logger.info("Loading %s laysers from %s", layer, path)
+        logger.info("Loading %s layers from %s", layer, path)
         ckpt = load_checkpoint(path=path, device=self.device)
         for k, v in ckpt["model_state"].items():
             if k.startswith(layer):